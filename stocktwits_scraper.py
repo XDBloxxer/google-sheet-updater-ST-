307823ef3661a3a3e91652837f1de6c499ea5223
XDBloxxer <xdbloxxer@gmail.com>
2025-01-12 04:10:54 +0200

    Update stocktwits_scraper.py

--- stocktwits_scraper.py
+++ stocktwits_scraper.py
@@ -14,7 +14,7 @@ def earnings():
     chrome = webdriver.Chrome(options=options)
 
     # Go to the earnings calendar page
-    chrome.get("https://stocktwits.com/markets/calendar")
+    chrome.get("https://stocktwits.com/rankings")
     
     # Get the page source (HTML content)
     htmlContent = chrome.page_source
@@ -77,7 +77,7 @@ def extract():
                 name = "topLosers"
             case 3:
                 # Corrected URL for trending stocks
-                url = "https://api.stocktwits.com/api/2/rankings/trending.json?limit=15"
+                url = "https://api.stocktwits.com/api/2/streams/trending.json?limit=15"
                 name = "trending"
         
         headers = {"User-Agent": "Mozilla/5.0"}
